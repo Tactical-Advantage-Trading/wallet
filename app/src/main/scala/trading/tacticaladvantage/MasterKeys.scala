d8847a0ff56bd5f952f4e08cf15fe6473e75b9c6
Anton <akumaigorodski@gmail.com>
2026-05-14 14:32:42 +0300

    Define sidechain key derivation rule

--- app/src/main/scala/trading/tacticaladvantage/MasterKeys.scala
+++ app/src/main/scala/trading/tacticaladvantage/MasterKeys.scala
@@ -1,24 +1,23 @@
 package trading.tacticaladvantage
 
-import fr.acinq.bitcoin.DeterministicWallet
-import fr.acinq.bitcoin.DeterministicWallet.ExtendedPrivateKey
+import trading.tacticaladvantage.Tools.{Bytes, StringList}
+import fr.acinq.bitcoin.DeterministicWallet._
 import fr.acinq.eclair.wire.CommonCodecs._
 import scodec.Codec
 import scodec.bits.ByteVector
 import scodec.codecs._
-import trading.tacticaladvantage.Tools.{Bytes, StringList}
 
 
 object MasterKeys {
   def fromSeed(seed: Bytes): MasterKeys = {
-    val bitcoinMaster = DeterministicWallet.generate(ByteVector.view(seed), "Bitcoin seed")
-    val tokenMaster = DeterministicWallet.generate(ByteVector.view(seed), "Token seed")
-    MasterKeys(bitcoinMaster, tokenMaster)
+    val bitcoinMaster = generate(ByteVector.view(seed), "Bitcoin seed")
+    val sideChainsMaster = generate(ByteVector.view(seed), "Sidechains superseed")
+    MasterKeys(bitcoinMaster, sideChainsMaster)
   }
 
   val masterKeysCodec: Codec[MasterKeys] = {
     (extendedPrivateKeyCodec withContext "bitcoinMaster") ::
-      (extendedPrivateKeyCodec withContext "tokenMaster")
+      (extendedPrivateKeyCodec withContext "sideChainsMaster")
   }.as[MasterKeys]
 
   val walletSecretCodec: Codec[WalletSecret] = {
@@ -28,5 +27,7 @@ object MasterKeys {
   }.as[WalletSecret]
 }
 
-case class MasterKeys(bitcoinMaster: ExtendedPrivateKey, tokenMaster: ExtendedPrivateKey)
 case class WalletSecret(keys: MasterKeys, mnemonic: StringList, seed: ByteVector)
+case class MasterKeys(bitcoinMaster: ExtendedPrivateKey, sideChainsMaster: ExtendedPrivateKey) {
+  def sideChainMaster(chainNum: Long): ExtendedPrivateKey = derivePrivateKey(sideChainsMaster, hardened(chainNum) :: 0L :: Nil)
+}
